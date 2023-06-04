55477084e7e8564f08ed3f697cc2bc4d5b39998f
Julieta <ochoajulieta67@gmail.com>
2023-06-04 19:28:21 -0300

    validacion form y estilos

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,39 @@
 
-
+console.log("hola mundo");
 let personas = [];
+let primero = document.querySelector("form input");
+primero.focus();
 
 function validar(){
-    let nombre = document.querySelector('#nombre');
-    let email = document.querySelector('#email');
+    let nombre = document.getElementById('nombre');
+    
+    let email = document.getElementById('email');
     let patron = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/;
-
+    let mensaje = document.getElementById('mensaje');
     if(nombre.value.trim()==="" || nombre.value.length < 3){
-        document.querySelector('#errorNombre').style.display = 'inline';
+        document.getElementById('errorNombre').style.display = 'inline';
+        nombre ="";
         nombre.focus;
         return false;
     }else{
-        document.querySelector('#errorNombre'.style.display = 'none');
+        document.getElementById('errorNombre'.style.display = 'none');
+    }
+    
+    if(!patron.test(email.value)){
+        document.getElementById('errorEmail').style.display = 'inline';
+        email.focus;
+        return false;
+    }else{
+        document.getElementById('errorEmail').style.display = 'none';
+        
+        
+    }
+    if(mensaje.value.trim() == ""){
+        document.getElementById('errorMensaje').style.display = 'inline';
+        return false;
+    }else{
+        document.getElementById('errorMensaje').style.display = 'none';
+        return false;
     }
     return false;
 }
-validar();
